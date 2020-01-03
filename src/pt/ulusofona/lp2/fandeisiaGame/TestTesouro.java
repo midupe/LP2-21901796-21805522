7c76797003a0928642f92befdaa5bc2c14044b98
Muhammad <a21805522@alunos.ulht.pt>
2020-01-03 13:24:15 +0000

    throws exception test getpontos

--- src/pt/ulusofona/lp2/fandeisiaGame/TestTesouro.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/TestTesouro.java
@@ -7,8 +7,13 @@ import static org.junit.Assert.*;
 public class TestTesouro {
 
     @Test
-    public void getId() {
+    public void getId() throws Exception {
         Tesouro t = new Tesouro(10,1,2,"gold");
         assertEquals(20,t.getId());
+    }
+
+    public void getPontos() throws Exception{
+        Tesouro x = new Tesouro(20,2,1,"silver");
+        assertEquals(0,x.getPontos());
     }
 }
