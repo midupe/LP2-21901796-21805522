bb0e5e8bde9e728b4e2a26bb409ba85f4cda75fc
Miguel Pereira <work.migueldp@gmail.com>
2020-01-19 00:13:48 +0000

    preparacao para parte3

--- src/pt/ulusofona/lp2/fandeisiaGame/InsufficientCoinsException.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/InsufficientCoinsException.java
@@ -1,5 +1,12 @@
 package pt.ulusofona.lp2.fandeisiaGame;
 
 public class InsufficientCoinsException extends Throwable {
-    
+    public boolean teamRequiresMoreCoins(int teamId){
+        //implementar
+        return false;
+    }
+    public int getRequiredCoinsForTeam(int teamID){
+        //Implementar
+        return 30;
+    }
 }
